6c4f6ae1bc72329b99a3cbb0ed756d3085462c79
Nao-98 <136950720+Nao-98@users.noreply.github.com>
2023-07-22 21:24:11 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+// 体重の変数の宣言
+let kadaiWeight;
+
+// 身長の変数の宣言
+let kadaiHeight;
+
+// BMIの変数の宣言
+let kadaiBmi;
+
+// 値の代入
+kadaiWeight = 68;
+
+// 値の代入
+kadaiHeight = 1.7;
+
+// BMIを算出する計算式
+kadaiBmi = (kadaiWeight / (kadaiHeight * kadaiHeight));
+
+// コンソールへの出力
+console.log(kadaiBmi);
